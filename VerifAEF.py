a01ddfb92397618e2816d7b5c50841e798d16781
Reight123 <clement_praud@yahoo.com>
2023-11-22 11:11:52 +0100

    Ajout de la verif AEF deter

--- VerifAEF.py
+++ VerifAEF.py
@@ -22,3 +22,30 @@ if est_complet(automate):
     print("L'automate est complet.")
 else:
     print("L'automate n'est pas complet.")
+
+def est_deterministe(automate):
+    for etat, transitions in automate.items():
+        symboles = set()
+        for symbole, destination in transitions.items():
+            # Vérifie si le symbole a déjà une transition depuis cet état
+            if symbole in symboles:
+                return False
+            symboles.add(symbole)
+    return True
+
+automate = {
+    'q0': {'a': 'q1', 'b': 'q2'},
+    'q1': {'a': 'q3', 'b': 'q4'},
+    'q2': {'a': 'q5', 'b': 'q6'},
+    'q3': {'a': 'q7', 'b': 'q8'},
+    'q4': {'a': 'q9', 'b': 'q10'},
+    'q5': {'a': 'q11', 'b': 'q12'},
+    'q6': {'a': 'q13', 'b': 'q14'},
+    'q7': {'a': 'q15', 'b': 'q16'},
+}
+
+if est_deterministe(automate):
+    print("L'automate est déterministe.")
+else:
+    print("L'automate n'est pas déterministe.")
+
