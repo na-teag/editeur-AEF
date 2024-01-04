ad6dc3251d236365303d0e0ff8f01cd37732ab25
cycy958 <baussartcy@cy-tech.fr>
2024-01-04 12:59:28 +0100

    mise à jour fichier langage et emonde

--- src/editing/emonde.py
+++ src/editing/emonde.py
@@ -1,9 +1,50 @@
+def est_emonde(automate): # verify if the automate is pruned
+    reachable_states = set()
 
+    def dfs(etat_actuel): # traverse the automate and mark reachable states
+        nonlocal reachable_states
+        reachable_states.add(etat_actuel)
 
-def est_emonde(automate): # Check if the given automaton is émondé, every state either is a final state or has outgoing transitions.
+        for etats_suivants in automate["Etats"][etat_actuel].values():
+            for etat_suivant in etats_suivants:
+                if etat_suivant not in reachable_states:
+                    dfs(etat_suivant)
+
+    for etat_initial in automate["Etats_initiaux"]:
+        dfs(etat_initial)
+
     for etat in automate["Etats"]:
-        if automate["Etats"][etat] == {} and etat not in automate["Etats_finaux"]:
+        if etat not in automate["Etats_finaux"] and not (
+            automate["Etats"][etat] == {} and etat in automate["Etats"][etat]
+        ):
             print("L'automate n'est pas émondé")
             return False
+
     print("L'automate est émondé")
     return True
+
+
+
+
+def rendre_emonde(automate): # Prunes the automaton by removing unreachable states.
+
+    def dfs(etat): # explores accessible states
+        nonlocal etats_accessibles
+        transitions = automate["Etats"].get(etat, {})
+        for etats_suivants in transitions.values():
+            for etat_suivant in etats_suivants:
+                if etat_suivant not in etats_accessibles:
+                    etats_accessibles.add(etat_suivant)
+                    dfs(etat_suivant)
+
+    etats_initiaux = set(automate["Etats_initiaux"])
+    etats_accessibles = set(etats_initiaux)
+
+    for etat_initial in etats_initiaux:
+        dfs(etat_initial)
+
+    automate["Etats"] = {etat: transitions for etat, transitions in automate["Etats"].items() if etat in etats_accessibles}
+    automate["Etats_initiaux"] = list(etats_initiaux.intersection(etats_accessibles))
+    automate["Etats_finaux"] = list(set(automate["Etats_finaux"]).intersection(etats_accessibles))
+
+    return automate
--- src/editing/langage.py
+++ src/editing/langage.py
@@ -1,31 +1,34 @@
 import data.file as file
 
 def generer_langage(automate): # Generate the language recognized by the given automaton.
-	langage = set()
-	etats_initiaux = automate["Etats_initiaux"]
+    langage = set()
+    etats_initiaux = automate["Etats_initiaux"]
 
-	def dfs(etat_actuel, mot_actuel):
-		nonlocal langage
-		transitions = automate["Etats"][etat_actuel]
-		
-		if mot_actuel == "":
-			if etat_actuel in automate["Etats_finaux"]:
-				langage.add("")
-		
-		for transition, etats_suivants in transitions.items():
-			for etat_suivant in etats_suivants:
-				dfs(etat_suivant, mot_actuel + transition)
-	
-	for etat_initial in etats_initiaux:
-		dfs(etat_initial, "")
-	
-	return langage
+    stack = [(etat_initial, "") for etat_initial in etats_initiaux]
+
+    while stack:
+        etat_actuel, mot_actuel = stack.pop()
+
+        transitions = automate["Etats"][etat_actuel]
+
+        if mot_actuel == "":
+            if etat_actuel in automate["Etats_finaux"]:
+                langage.add("")
+
+        for transition, etats_suivants in transitions.items():
+            for etat_suivant in etats_suivants:
+                stack.append((etat_suivant, mot_actuel + transition))
+
+    return langage
+
+
 
 def automates_equivalents(automate1, automate2): # Check if two automata are equivalent, i.e., recognize the same language.
 	langage1 = generer_langage(automate1)
 	langage2 = generer_langage(automate2)
 
 	return langage1 == langage2
+
 
 
 def test_automates_equivalents(liste, num_automate): # Choose the 2nd automaton and run the automates_equivalents() function
