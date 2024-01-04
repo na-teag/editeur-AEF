4c71c2e280ccae4bacb0cb86a20eaf5d161ca429
Reight123 <clement_praud@yahoo.com>
2024-01-04 16:19:36 +0100

    rendredeter nettoyage

--- src/editing/deter.py
+++ src/editing/deter.py
@@ -57,7 +57,6 @@ def rendredeterministe(automate):
     for etat, transitions in etats.items(): # loops for each etat
     
         if etat in i:
-            print("on rentre dans l'etat ini")
             automate0["Etats"][etat] = {}
             for transi, etatfin in transitions.items():
                 if len(etatfin) > 1:
@@ -65,114 +64,57 @@ def rendredeterministe(automate):
                     automate0["Etats"][etatfinc] = {}
                     
                     nouvetat.append(etatfinc)
-                    print(nouvetat)
                     
                     etatfinl = [','.join(etatfin)]
                     automate0["Etats"][etat][transi] = etatfinl
-                    
-                    print("fin")
                 else:
                     automate0["Etats"][etat][transi] = etatfin
                     etatfinc = ','.join(etatfin)
                     nouvetat.append(etatfinc)
-                    print(nouvetat)
                     automate0["Etats"][etatfinc] = {}
-    print("fin etat ini")
-    print(nouvetat)
-    print("-----------------------------------------------")
     while len(nouvetat) > 0 :
         etat_rechercher = nouvetat[0]
-        print(automate0["Etats"]["1,3"])
-        print(nouvetat)
         etat_rechercherc = [nouvetat.pop(0)]
-        print("************************************************************************************************************")
-        print(etat_rechercherc[0])
-        print(nouvetat)
         etat_rechercherl = list(map(str, etat_rechercherc[0].split(',')))
-        print(etat_rechercherl)
-        print("*************************************************************************************************************")
-        
         if len(etat_rechercherl) == 1:
             for etat, transitions in etats.items():
                 if etat in etat_rechercherl:
-                    print("on rentre dans l'etat nouv")
-                    print("----")
-                    print(automate0["Etats"]["1,3"])
                     for transi, etatfin in transitions.items():
                         etatfinl = [','.join(etatfin)]
                         etatfinc = ','.join(etatfin)
-                        print("1")
-                        print(automate0["Etats"]["1,3"])
                         if len(etatfin) > 1:
                             automate0["Etats"][etat][transi] = etatfinl
                             if etatfinc not in automate0["Etats"]:
                                 automate0["Etats"][etatfinc] = {}
                                 nouvetat.append(etatfinc)
-                                print(nouvetat)
                         else :
-                            print(automate0["Etats"]["1,3"])
                             automate0["Etats"][etat][transi] = etatfin
-                            print(automate0["Etats"]["1,3"])
                             if etatfinc not in automate0["Etats"]:
                                 automate0["Etats"][etatfinc] = {}
                                 nouvetat.append(etatfin)
-                                print(nouvetat)
-            print(automate0["Etats"]["1,3"])
-            print("fin etat rajouté")
-            print(nouvetat)
         else :
-            print("osjidnfgsgf")
             for etat, transitions in etats.items():
-                print("boucleetat")
                 if etat in etat_rechercherl:
-                    print("on rentre dans l'etat relou")
-                    print("----")
                     for transi, etatfin in transitions.items():
-                        print("hm")
                         etatfinl = [','.join(etatfin)]
                         etatfinc = ','.join(etatfin)
-                        print(automate0["Etats"][etat_rechercherc[0]])
-                        print(automate0["Etats"]["1,3"])
-                        print(etat_rechercherc)
-                        print(etat_rechercherl)
-                        print(etat_rechercherc[0])
-                        print("\nAutomate converti :")
-                        print(automate0)
-                        #if etat_rechercherc[0] in automate0["Etats"]:
-                            #print("l'etat existe")
                         if transi not in automate0["Etats"][etat_rechercherc[0]]:
-                            print("mais pas la transi")
-                            print(etatfin)
                             if len(etatfin) > 1:
                                 automate0["Etats"][etat_rechercherc[0]][transi] = etatfinl
                                 if etatfinc not in automate0["Etats"]:
                                     automate0["Etats"][etatfinc] = {}
                                     nouvetat.append(etatfinc)
-                                    print(nouvetat)
                             else :
                                 automate0["Etats"][etat_rechercherc[0]][transi] = etatfin
                                 if etatfinc not in automate0["Etats"]:
                                     automate0["Etats"][etatfinc] = {}
                                     nouvetat.append(etatfin)
-                                    print(nouvetat)
-                                print("transi+etatfin créer")
                         else :
-                            print("la transi existe")
-                            print(automate0["Etats"]["1,3"])
-                            print(automate0["Etats"][etat_rechercherc[0]][transi])
-                            print(etatfin)
-                            print(etatfinl)
-                            print(etat_rechercherc[0])
                             temp = ','.join(automate0["Etats"][etat_rechercherc[0]][transi])
-                            print(temp[0])
                             etatcreer = list(map(str, temp.split(',')))
-                            print(etatcreer)
-                            print(etatfinc)
-                            print("--------")
                             if etatfinc not in etatcreer:
                                 etatcomplet = [",".join(etatfinl+automate0["Etats"][etat_rechercherc[0]][transi])]
                                 automate0["Etats"][etat_rechercherc[0]][transi] = etatcomplet
-                                print(automate0["Etats"]["1,3"])
                             
         etats0 = automate0["Etats"]
         u = '"' + str(etat_rechercher) + '"'
@@ -180,17 +122,11 @@ def rendredeterministe(automate):
             if etat in u:
                 for transi, etatfin in transitions.items():
                     etatfinc = ','.join(etatfin)
-                    print("ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo")
-                    
-                    print(u)
-                    print(etatfinc)
-                    print(automate0["Etats"])
                     if etatfinc not in etats0:
                         listechgmt.append(etatfinc)
         while listechgmt :
             automate0["Etats"][listechgmt[0]] = {}
             nouvetat.append(listechgmt[0])
-            print(nouvetat)
             listechgmt.pop(0)
     print("\nAutomate converti :")
     print(automate0)                           
