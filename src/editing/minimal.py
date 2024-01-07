005981d512d52fc07d631a30470db02dcc778d20
zeynebcha <zeineb.chaabane98@gmail.com>
2024-01-07 20:46:04 +0100

    upates

--- src/editing/minimal.py
+++ src/editing/minimal.py
@@ -94,27 +94,6 @@ def toMinimal(automate: dict):
     return newAutomate
 
 
-autommate = {
-	"Etats": {
-		"q0": {"a": ["q2"], "b": ["q1"]},
-		"q1": {"a": ["q2"], "b": ["q1"]},
-		"q2": {"a": ["q3"], "b": ["q2"]},
-		"q3": {"a": ["q5"], "b": ["q4"]},
-        "q4": {"a": ["q5"], "b": ["q4"]},
-		"q5": {"a": ["q6"], "b": ["q5"]},
-		"q6": {"a": ["q5"], "b": ["q7"]},
-		"q7": {"a": ["q5"], "b": ["q7"]}
-	},
-	"Etats_initiaux": ["q0"],
-	"Etats_finaux": ["q0","q3", "q4", "q6", "q7"],
-	"Nom" : "test"
-}
-print(toMinimal(autommate))
-
-
-
-
-
 
 def minimal(liste, num_automate):
     automate = liste[num_automate]
