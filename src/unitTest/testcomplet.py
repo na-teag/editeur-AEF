ebf4af0b03fbba57befd2c7f42fbd5c979333285
na-teag <gaetan.g237@gmail.com>
2024-01-10 17:31:14 +0100

    correction test complet

--- src/unitTest/testcomplet.py
+++ src/unitTest/testcomplet.py
@@ -1,30 +1,34 @@
 from editing.complet import rendrecomplet, est_complet
 
-automate ={
-    "Etats": {
-        "1": {"a": ["2"], "b": ["2"]},
-        "2": {"b": ["3"]},
-        "3": {"b": ["2"]}
-    },
-    "Etats_initiaux": ["1"],
-    "Etats_finaux": ["3"],
-    "Nom" : "automate"
-}
+def get_automate():
+    automate ={
+        "Etats": {
+            "1": {"a": ["2"], "b": ["2"]},
+            "2": {"b": ["3"]},
+            "3": {"b": ["2"]}
+        },
+        "Etats_initiaux": ["1"],
+        "Etats_finaux": ["3"],
+        "Nom" : "automate"
+    }
+    return automate
 
-automatevoulu ={
-    "Etats": {
-        "1": {"a": ["2"], "b": ["2"]},
-        "2": {"b": ["3"], "a": ["2"]},
-        "3": {"b": ["2"], "a": ["3"]}
-    },
-    "Etats_initiaux": ["1"],
-    "Etats_finaux": ["3"],
-    "Nom" : "automate"
-}
+def get_automate_voulu():
+    automatevoulu ={
+        "Etats": {
+            "1": {"a": ["2"], "b": ["2"]},
+            "2": {"b": ["3"], "a": ["2"]},
+            "3": {"b": ["2"], "a": ["3"]}
+        },
+        "Etats_initiaux": ["1"],
+        "Etats_finaux": ["3"],
+        "Nom" : "automate"
+    }
+    return automatevoulu
 
 def test_verifcomplet():
-    if est_complet(automatevoulu):
-        if est_complet(automate):
+    if est_complet(get_automate_voulu()):
+        if est_complet(get_automate()):
             return False
         else :
             return True
@@ -32,9 +36,10 @@ def test_verifcomplet():
         return False
 
 def test_complet():
-    nautomate = rendrecomplet(automate)
+    nautomate = rendrecomplet(get_automate())
 
-    if nautomate == automatevoulu:
+    if nautomate == get_automate_voulu():
         return True
     else:
         return False
+
